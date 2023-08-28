78f2cf22f731cecd99f43134069578a8b5b1aceb
Miguel <miguellofredo@gmail.com>
2023-08-28 14:11:56 -0300

    entity User with room db

---
+++ app/src/main/java/com/example/contactapp/room/User.kt
@@ -0,0 +1,16 @@
+package com.example.contactapp.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "user_id")
+    val id:Int,
+    @ColumnInfo(name = "user_name")
+    val name:String,
+    @ColumnInfo(name="user_email")
+    val email:String
+)
